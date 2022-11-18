03a0644736931a19cb68323042e8300047e4f78f
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2022-11-18 21:28:22 +0000

    change toString function to deeply inspect Objects and Arrays

--- index.test.ts
+++ index.test.ts
@@ -1,48 +1,89 @@
-import {assert, describe, test} from "vitest";
-import {element, nest, set} from "./index";
+import { assert, describe, test } from 'vitest'
+import { element, nest, set } from './index'
 
 describe('set function', () => {
-    test('will generate the attribute pair string', () => {
-        assert.equal(set({ key: 'value'}), ' key="value"')
-    })
+  test('will generate the attribute pair string', () => {
+    assert.equal(set({ key: 'value' }), ' key="value"')
+  })
 
-    test('will have space between pair strings', () => {
-        assert.equal(set({ key1: 'value1', key2: 'value2'}), ' key1="value1" key2="value2"')
-    })
+  test('will have space between pair strings', () => {
+    assert.equal(
+      set({ key1: 'value1', key2: 'value2' }),
+      ' key1="value1" key2="value2"'
+    )
+  })
 
-    test('will force values to be strings', () => {
-        assert.equal(set({ function: () => () => {}, boolean: true, object: { key: "value"}, array: ['first', 'second']}), ` function="() => {\n    }" boolean="true" object="{ key: 'value' }" array="[ 'first', 'second' ]"`)
-    })
+  test('will force values to be strings', () => {
+    assert.equal(
+      set({
+        function: () => () => {},
+        boolean: true,
+        object: {
+          string: 'string',
+          boolean: true,
+          object: { key: 'value' },
+          array: [],
+          function: () => {}
+        },
+        array: ['string', true, { key: 'value' }, () => {}]
+      }),
+      ` function="() => { }" boolean="true" object="{ string: 'string', boolean: true, object: { key: 'value' }, array: [], function: () => { } }" array="[ 'string', true, { key: 'value' }, () => { } ]"`
+    )
+  })
 })
 
 describe('nest function', () => {
-    test('will generate string of children', () => {
-        assert.equal(nest('text', '<child>'), 'text<child>')
-    })
+  test('will generate string of children', () => {
+    assert.equal(nest('text', '<child>'), 'text<child>')
+  })
 })
 
 describe('element function', () => {
-    test('will generate an element string', () => {
-        assert.equal(element('element'), '<element></element>')
-    })
+  test('will generate an element string', () => {
+    assert.equal(element('element'), '<element></element>')
+  })
 
-    test('can generate a selfClosing element string', () => {
-        assert.equal(element('element', true), '<element>')
-    })
+  test('can generate a selfClosing element string', () => {
+    assert.equal(element('element', true), '<element>')
+  })
 
-    test('can add attributes to an element string', () => {
-        assert.equal(element('element', false, { key1: 'value1', key2: 'value2'}), '<element key1="value1" key2="value2"></element>')
-    })
+  test('can add attributes to an element string', () => {
+    assert.equal(
+      element('element', false, { key1: 'value1', key2: 'value2' }),
+      '<element key1="value1" key2="value2"></element>'
+    )
+  })
 
-    test('can add attributes to a selfClosing element string', () => {
-        assert.equal(element('element', true, { key1: 'value1', key2: 'value2'}), '<element key1="value1" key2="value2">')
-    })
+  test('can add attributes to a selfClosing element string', () => {
+    assert.equal(
+      element('element', true, { key1: 'value1', key2: 'value2' }),
+      '<element key1="value1" key2="value2">'
+    )
+  })
 
-    test('can add children to an element string', () => {
-        assert.equal(element('element', false, { key1: 'value1', key2: 'value2'}, 'text', '<child>'), '<element key1="value1" key2="value2">text<child></element>')
-    })
+  test('can add children to an element string', () => {
+    assert.equal(
+      element(
+        'element',
+        false,
+        { key1: 'value1', key2: 'value2' },
+        'text',
+        '<child>'
+      ),
+      '<element key1="value1" key2="value2">text<child></element>'
+    )
+  })
 
-    test("won't add children to a selfClosing element string", () => {
-        assert.equal(element('element', true, { key1: 'value1', key2: 'value2'}, 'text', '<child>'), '<element key1="value1" key2="value2">')
-    })
+  test("won't add children to a selfClosing element string", () => {
+    assert.equal(
+      element(
+        'element',
+        true,
+        { key1: 'value1', key2: 'value2' },
+        'text',
+        '<child>'
+      ),
+      '<element key1="value1" key2="value2">'
+    )
+  })
 })
--- index.ts
+++ index.ts
@@ -1,25 +1,52 @@
-import * as util from "util";
+import * as util from 'util'
 
-export const toString = function(item: any): string {
-    if(item.constructor === Object || Array.isArray(item)) return util.inspect(item)
-    return item.toLocaleString()
+export const toString = function (item: any): string {
+  if (item.constructor === Object)
+    return Object.entries(item).reduce(
+      (acc, [_key, value]) =>
+        typeof value === 'function'
+          ? acc.replace(util.inspect(value), value.toString())
+          : acc,
+      util.inspect(item)
+    )
+  if (Array.isArray(item))
+    return item.reduce(
+      (acc, curr) =>
+        typeof curr === 'function'
+          ? acc.replace(util.inspect(curr), curr.toString())
+          : acc,
+      util.inspect(item)
+    )
+  return item.toString()
 }
 
 export const set = function (attributes: Object | (() => Object)): string {
-    return Object.entries(attributes).reduce((acc, [key, value]) => `${acc} ${key}="${
-        typeof value === 'function' ? toString(value()) : toString(value)
-    }"`,'')
+  return Object.entries(attributes).reduce(
+    (acc, [key, value]) =>
+      `${acc} ${key}="${
+        typeof value === 'function'
+          ? toString(value()).replace(/\s+/gm, ' ')
+          : toString(value).replace(/\s+/gm, ' ')
+      }"`,
+    ''
+  )
 }
 
-export const nest = function (...children: (string | (() => string))[]): string {
-    return children.map(child => typeof child === 'function' ? child() : child).join('')
+export const nest = function (
+  ...children: (string | (() => string))[]
+): string {
+  return children
+    .map((child) => (typeof child === 'function' ? child() : child))
+    .join('')
 }
 
 export const element = function (
-    tagName: string,
-    selfClose: boolean = false,
-    attributes?: Object | (() => Object),
-    ...children: (string | (() => string))[]
+  tagName: string,
+  selfClose: boolean = false,
+  attributes?: Object | (() => Object),
+  ...children: (string | (() => string))[]
 ): string {
-    return selfClose ? `<${tagName + set(attributes ?? {})}>` : `<${tagName + set(attributes ?? {})}>${nest(...children)}</${tagName}>`
+  return selfClose
+    ? `<${tagName + set(attributes ?? {})}>`
+    : `<${tagName + set(attributes ?? {})}>${nest(...children)}</${tagName}>`
 }
